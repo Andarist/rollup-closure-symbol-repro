17b75867b7a9fc454a9f8f69fe2bb036973e856d
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-09-19 10:49:19 +0200

    Working repro 🚀

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const SMTH = Symbol.for('smth')
 
-export const isSmth = smth => smth && smth[SMTH]
+var hasSymbol = typeof Symbol === 'function' && Symbol.for;
+
+var SMTH = hasSymbol ? Symbol.for('smth') : '@@smth';
+
+// this version causes the "polyfill bloat" to be added to both cjs & esm outputs
+//
+// const isSmth = (smth) => {
+//   return smth && !!smth[SMTH]
+// }
+
+function isSmth(smth) {
+  return smth && !!smth[SMTH]
+}
+
+export { isSmth }
